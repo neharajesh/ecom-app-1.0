ec58232fcb64ae3c1d2b5fe3a1be5cf38de06acb
neharajesh <leehanseok3@gmail.com>
2021-05-06 04:40:20 +0530

    update - style updates

--- src/Layout/Sidebar/Sidebar.js
+++ src/Layout/Sidebar/Sidebar.js
@@ -1,13 +1,17 @@
 import "../layout.css";
 import { RiHomeLine } from "react-icons/ri";
-import { IoMdList } from "react-icons/io";
-import { VscAccount } from "react-icons/vsc";
 import { FiShoppingCart } from "react-icons/fi";
 import { FaRegHeart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import { AiOutlineDropbox } from "react-icons/ai";
+import { BiLogIn, BiLogOut } from "react-icons/bi";
+import { useAuthDispatch, useAuthState } from "../../auth/auth-context";
+import { logout } from "../../auth/auth-actions";
 
 export const Sidebar = ({ sidebar }) => {
+  const user = useAuthState();
+  const dispatch = useAuthDispatch();
+
   return (
     <>
       <nav className={sidebar ? "sidebar open" : "sidebar"}>
@@ -27,35 +31,44 @@ export const Sidebar = ({ sidebar }) => {
         >
           <AiOutlineDropbox size={30} /> <span>Products</span>
         </NavLink>
-        {/* add navlink for the rest of these */}
-        <li className="nav-link">
-          <IoMdList size={30} /> <span>Categories</span>
-        </li>
+        <NavLink to="/cart" className="nav-link" activeClassName="nav-active">
+          <FiShoppingCart size={30} /> <span>Cart</span>
+        </NavLink>
+        <NavLink
+          to="/wishlist"
+          className="nav-link"
+          activeClassName="nav-active"
+        >
+          <FaRegHeart size={30} /> <span>Wishlist</span>
+        </NavLink>
+
         <div className="sidebarOnly">
-          <NavLink
-            to="/login"
-            className="nav-link"
-            activeClassName="nav-active"
-          >
-            <FaRegHeart size={30} /> <span>Login</span>
-          </NavLink>
+          {user?.token === "" ? (
+            <NavLink
+              to="/auth/signin"
+              className="nav-link"
+              activeClassName="nav-active"
+            >
+              <BiLogIn size={30} /> <span>Login</span>
+            </NavLink>
+          ) : (
+            <NavLink
+              onClick={() => logout(dispatch)}
+              to="/"
+              className="nav-link"
+              activeClassName="nav-active"
+            >
+              <BiLogOut size={30} /> <span>Logout</span>
+            </NavLink>
+          )}
+          {/* TO ADD LATER 
           <NavLink
             to="/profile"
             className="nav-link"
             activeClassName="nav-active"
           >
             <VscAccount size={30} /> <span>Profile</span>
-          </NavLink>
-          <NavLink to="/cart" className="nav-link" activeClassName="nav-active">
-            <FiShoppingCart size={30} /> <span>Cart</span>
-          </NavLink>
-          <NavLink
-            to="/wishlist"
-            className="nav-link"
-            activeClassName="nav-active"
-          >
-            <FaRegHeart size={30} /> <span>Wishlist</span>
-          </NavLink>
+          </NavLink> */}
         </div>
       </nav>
     </>
--- src/Layout/header/Header.js
+++ src/Layout/header/Header.js
@@ -6,11 +6,11 @@ import { logout } from "../../auth/auth-actions";
 
 export const Header = ({ handleToggleSidebar }) => {
   const user = useAuthState();
-  console.log(user);
   const dispatch = useAuthDispatch();
   return (
     <>
       <div className="header flex flex-space-between flex-items-center">
+        <div>{/* To keep the header in the center */}</div>
         <GiHamburgerMenu
           className="header-menu"
           size={35}
@@ -20,19 +20,8 @@ export const Header = ({ handleToggleSidebar }) => {
         <Link to="/" className="header-brand txt-700">
           PETMART
         </Link>
-        {/* <div className="header-brand txt-700">PET MART</div> */}
 
         <div className="header-links flex">
-          <NavLink to="/cart" className="nav-link" activeClassName="nav-active">
-            Cart
-          </NavLink>
-          <NavLink
-            to="/wishlist"
-            className="nav-link"
-            activeClassName="nav-active"
-          >
-            Wishlist
-          </NavLink>
           {user?.token === "" ? (
             <NavLink
               to="/auth/signin"
