3ff4cac0632322cf5901815a2c88c7bad20bcb97
neharajesh <leehanseok3@gmail.com>
2021-05-06 21:59:36 +0530

    changed rooturl

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 //for localhost
-// const ROOT_URL = "http://localhost:5000";
+const ROOT_URL = "http://localhost:5000";
 
 //for Repl
-const ROOT_URL = "https://ecommerce-application-backend.neharajesh.repl.co";
+// const ROOT_URL = "https://ecommerce-application-backend.neharajesh.repl.co";
 
 export default ROOT_URL;
